63c095d9d5d8672614232fbc34967e0eb2bff0fa
KunYi Chen <kunyi.chen@gmail.com>
2024-03-15 18:30:33 +0800

    update FreeRTOS and printf

--- Core/Src/gcc_printf_port.c
+++ Core/Src/gcc_printf_port.c
@@ -1,20 +1,23 @@
 #include <stm32f4xx.h>
 
+int __io_putchar(int ch)
+{
+  while ((USART3->SR & 0x40) == 0) {}
+  if (ch == '\n') {
+    USART3->DR = '\r';
+    while ((USART3->SR & 0x40) == 0) {}
+  }
+  USART3->DR = (uint8_t) ch;
+  return ch;
+}
+
 int _write (int fd, char *pBuffer, int size)
 {
   for(int i = 0; i < size; i++) {
-	  do {
-      ;
-    } while((USART3->SR & 0x40) == 0);
-	  USART3->DR = (uint8_t) pBuffer[i];
+    __io_putchar(pBuffer[i]);
   }
   return size;
 }
 
 
-int __io_putchar(int ch)
-{
-  while ((USART3->SR & 0x40) == 0) {}
-  USART3->DR = (uint8_t) ch;
-  return ch;
-}
+
